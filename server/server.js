6f38fb17007e408d935e34183995ac5deef03b78
Alice Smith <alice@webage.com>
2024-07-02 14:19:59 -0400

    Created routes

--- server/server.js
+++ server/server.js
@@ -12,9 +12,88 @@ const collectionNames = process.env.MONGO_DB_COLLECTION.split(',');
 const PORT = 3001;
 const app = express();
 
+
+
+app.get('/api/characters', async (req, res) => {
+    try {
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection(collectionNames[0]);
+        console.log(collectionNames)
+        const characters = await collection.find({}).toArray();
+        res.json(characters);
+    } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("Hmmm, something smells... Error fetching characters");
+    }
+});
+
+app.get('/api/films', async (req, res) => {
+    try {
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection(collectionNames[1]);
+        console.log(collectionNames)
+        const films = await collection.find({}).toArray();
+        res.json(films);
+    } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("Hmmm, something smells... Error fetching films");
+    }
+});
+
+
 app.get("/api/planets", async (req, res) => {
-    console.log("Server is running!");
-    res.json({hello: "Hello Worlds!"})
+    try {
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection(collectionNames[2]);
+        console.log(collectionNames)
+        const planets = await collection.find({}).toArray();
+        res.json(planets);
+    } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("Hmmm, something smells... Error fetching planets");
+    }
+});
+
+app.get("/api/characters/:id", async (req, res) => {
+    try {
+        const characterId = +req.params.id;
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection(collectionNames[0]);
+        const characters = await collection.find({'id': characterId}).toArray();
+        res.json(characters);
+    } catch (err) {
+        console.error('Error:', err);
+        res.status(500).send('Hmm, something doesn\'t smell right... Error fetching characters/id');
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+app.get("/api/films/:id", async (req, res) => {
+    try {
+        const filmId = +req.params.id;
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection(collectionNames[1]);
+        const films = await collection.find({'id': filmId}).toArray();
+        res.json(films);
+    } catch (err) {
+        console.error('Error:', err);
+        res.status(500).send('Hmm, something doesn\'t smell right... Error fetching film/id');
+    }
 });
 
 app.listen(PORT, () => {
