29affb21812f7a4304f0460701df3f0e0a391c0d
Ian Grzembski <iangrz2003@gmail.com>
2024-07-02 11:22:25 -0400

    Imported JSON data into MongoDB

---
+++ mongoimport.sh
@@ -0,0 +1,13 @@
+JSON_DIR="c:/swapi/json-data"
+MONGO_URI="mongodb://localhost:27017/swapi"
+ 
+ 
+for json_file in $JSON_DIR/*.json; do
+ 
+    collection_name=$(basename "$json_file" .json)
+ 
+    mongoimport --uri $MONGO_URI --collection $collection_name --drop --file $json_file --jsonArray
+done
+ 
+#chmod +x mongoimport.sh
+#/mongoimport.sh
